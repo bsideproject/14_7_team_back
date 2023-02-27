978f84725f2847e581cef0b871436ca78f663c1f
kimtm23 <kimtm23@gmail.com>
2023-02-27 11:00:55 +0900

    init commit

---
+++ src/main/java/com/example/mineservice/MineServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.mineservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MineServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MineServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mineservice/MineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mineservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MineServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
