ceb1464cf74192bf754678a1b14c86105739efb0
Hayden <a99048925@gmail.com>
2023-03-09 11:13:34 +0900

    fix : 테스트 api 및 swagger 설정

--- src/main/java/com/example/mineservice/TestController.java
+++ src/main/java/com/example/mineservice/TestController.java
@@ -1,14 +1,18 @@
 package com.example.mineservice;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@Api(tags = "테스트")
 public class TestController {
 
     @GetMapping("/test")
+    @ApiOperation(value = "테스트 API 입니다")
     public String test(){
         return "server started";
     }
