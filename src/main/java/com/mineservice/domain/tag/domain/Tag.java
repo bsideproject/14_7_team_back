2913d6c7c14c805973bb8dd752c408896c03ddee
Hayden <a99048925@gmail.com>
2023-03-22 14:45:04 +0900

    fix : merge 오류 수정

--- src/main/java/com/mineservice/domain/tag/domain/Tag.java
+++ src/main/java/com/mineservice/domain/tag/domain/Tag.java
@@ -36,10 +36,6 @@ public class Tag {
     @JoinColumn(name = "user_id", insertable = false, updatable = false)
     private UserInfo userInfo;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id", insertable = false, updatable = false)
-    private UserInfo userInfo;
-
     @OneToMany(mappedBy = "article")
     List<ArticleTag> articleTags;
 
