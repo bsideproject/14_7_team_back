0f3baedd1010b41e6d8184056788d5c6c3fff2b1
ohg0219 <a99048925@gmail.com>
2023-03-27 23:38:52 +0900

    feat : 운영환경에서의 swagger 테스트를 위한 config 추가

--- src/main/java/com/mineservice/global/config/SwaggerConfig.java
+++ src/main/java/com/mineservice/global/config/SwaggerConfig.java
@@ -13,10 +13,12 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Server;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Collections;
 import java.util.List;
 
 import static springfox.documentation.schema.AlternateTypeRules.newRule;
@@ -35,7 +37,10 @@ public class SwaggerConfig {
 
     @Bean
     public Docket restAPI() {
+        Server localServer = new Server("local", "http://localhost:8080", "for local", Collections.emptyList(), Collections.emptyList());
+        Server mineServer = new Server("mine", "https://mine.directory", "for prod", Collections.emptyList(), Collections.emptyList());
         return new Docket(DocumentationType.OAS_30)
+                .servers(localServer, mineServer)
                 .useDefaultResponseMessages(false)
                 .alternateTypeRules(newRule(typeResolver.resolve(Pageable.class), typeResolver.resolve(Page.class)))
                 .apiInfo(apiInfo())
