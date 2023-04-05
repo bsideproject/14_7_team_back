2a375cad7b1da19e3ce03b704e0203c612cde8af
ohg0219 <a99048925@gmail.com>
2023-04-05 22:24:00 +0900

    fix : 아티클 api 시큐리티 적용

--- src/main/java/com/mineservice/domain/article/dto/ArticleResDTO.java
+++ src/main/java/com/mineservice/domain/article/dto/ArticleResDTO.java
@@ -2,11 +2,13 @@ package com.mineservice.domain.article.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.util.List;
 
 @Getter
 @Builder
+@ToString
 public class ArticleResDTO {
 
     private Long totalArticleSize;
