8a7f0e1b3e8799897e8de0804f9f804232a87aa0
ohg0219 <a99048925@gmail.com>
2023-05-15 00:04:39 +0900

    fix : article list 조회시 count query 오류 수정

--- src/main/java/com/mineservice/domain/article/repository/ArticleRepository.java
+++ src/main/java/com/mineservice/domain/article/repository/ArticleRepository.java
@@ -28,7 +28,18 @@ public interface ArticleRepository extends JpaRepository<ArticleEntity, Long> {
             "AND (:readYn is null OR a.read_yn = :readYn) " +
             "AND (:types is null OR a.type IN (:types)) " +
             "AND (:tags is null OR t.name IN (:tags)) " +
-            "GROUP BY a.id ", nativeQuery = true)
+            "GROUP BY a.id ", nativeQuery = true,
+            countQuery = "SELECT COUNT(distinct a.id) " +
+                    "FROM article a " +
+                    "LEFT JOIN article_tag atag ON a.id = atag.article_id " +
+                    "LEFT JOIN tag t ON t.id = atag.tag_id " +
+                    "WHERE a.user_id = :userId " +
+                    "AND (:title is null OR a.title LIKE %:title%) " +
+                    "AND (:favorite is null OR a.favorite = :favorite) " +
+                    "AND (:readYn is null OR a.read_yn = :readYn) " +
+                    "AND (:types is null OR a.type IN (:types)) " +
+                    "AND (:tags is null OR t.name IN (:tags)) " +
+                    "GROUP BY a.user_id ")
     Page<ArticleEntity> findAllBySearch(@Param("title") String title,
                                         @Param("favorite") String favorite,
                                         @Param("readYn") String readYn,
